8af5920307b927a474ea8a5558d54e828689596b
strella111 <koliesnikov-maksim@mail.ru>
2025-10-06 17:20:59 +0300

    Вынос диалога настроек в отдельный файл

--- src/ui/dialogs/__init__.py
+++ src/ui/dialogs/__init__.py
@@ -5,8 +5,12 @@ This package contains all the dialog windows for the application.
 """
 
 from .pna_file_dialog import PnaFileDialog
+from .settings_dialog import SettingsDialog
+from .add_coord_syst_dialog import AddCoordinateSystemDialog
 
 __all__ = [
-    'PnaFileDialog'
+    'PnaFileDialog',
+    'SettingsDialog',
+    'AddCoordinateSystemDialog'
 ]
 
